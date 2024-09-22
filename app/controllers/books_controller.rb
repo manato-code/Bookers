83d620f85aa299cb2cf563ca3a49a6cf65348af4
ManatoYamakoshi <178896871+manato-code@users.noreply.github.com>
2024-09-22 13:31:22 +0000

    [Add] flashメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def index
@@ -19,6 +24,23 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end 
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books/index'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'books/new' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/:id' => 'books#show'
+  get 'books/index' => 'books#index'
+  post 'books/' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   root to: 'homes#top'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
