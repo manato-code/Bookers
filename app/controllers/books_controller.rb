d5981ee2bcd96c1fa95f0a5dab0c09fdfd6cf374
ManatoYamakoshi <178896871+manato-code@users.noreply.github.com>
2024-09-24 14:41:44 +0000

    [Finish]All

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,9 +38,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books/index'
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
